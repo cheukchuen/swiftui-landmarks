362317040a73d0cf08d261705854b01b4e4d600d
Cheuk Chuen Siow <11702891+cheukchuen@users.noreply.github.com>
2021-05-01 16:20:46 -0400

    Set up navigation between list and detail

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -2,8 +2,13 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
