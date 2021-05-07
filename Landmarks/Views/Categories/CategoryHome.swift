80faccaf4cc677cbcf5f13f24ea5989dc9fb8e5b
Cheuk Chuen Siow <11702891+cheukchuen@users.noreply.github.com>
2021-05-06 22:30:19 -0400

    Add category view

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
